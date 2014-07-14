eb14b9e4aae395cd5cc8d2edc4d9a789ce5cd8eb
fernando <fernando@BlueMeth>
2014-07-14 16:54:19 +0100

    HelpDialog- Improvements to the window, now doesn't have any buttons and closes pressing enter

--- src/jwalletUI/HelpDialog.java
+++ src/jwalletUI/HelpDialog.java
@@ -9,8 +9,11 @@ package jwalletUI;
 import java.awt.BorderLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 
@@ -21,6 +24,7 @@ import javax.swing.JLabel;
 public class HelpDialog extends JDialog{
     
     JLabel info, image;
+    JButton exit;
     
     public HelpDialog() {
         super();
@@ -28,22 +32,38 @@ public class HelpDialog extends JDialog{
         setLocationRelativeTo(null);
         setTitle("Help");
         setLayout(new BorderLayout());
+        setResizable(true);
+        setUndecorated(true);
+        
+        
         
         Image img = Toolkit.getDefaultToolkit().createImage("src/resources/help.png");
         image = new JLabel(new ImageIcon(img));
         image.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
         
         info = new JLabel("<html><center>JWallet is an application which helps you manage your money."
-                + "First, you have to create the wallet, with your name, "
+                + " First, you have to create the wallet, with your name, "
                 + "password and balance. After that you can manage the wallet,"
-                + "making deposits, withdraws and checking your logs.</center></html>");
+                + "making deposits, withdraws and checking your logs.<br><br>"
+                + "<font size=\"1\">press enter to close</font></center></html>");
         info.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        
+        exit = new JButton();
+        exit.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                dispose();
+            }
+        });
+        exit.setVisible(false);
         
         add(image, BorderLayout.NORTH);
         add(info, BorderLayout.CENTER);
-        
+        add(exit, BorderLayout.SOUTH);
         setModal(true);
         
+        getRootPane().setDefaultButton(exit);
         
         setVisible(true);
     }
